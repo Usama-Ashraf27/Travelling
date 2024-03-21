2c57f2f8ce413a1b10f206d17fe3152daec53716
Usama-Ashraf27 <usama@karigar.pk>
2024-03-20 20:34:00 -0700

    Accounts

--- Screens/HomeScreens/Account.js
+++ Screens/HomeScreens/Account.js
@@ -1,25 +1,64 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 import Layout from "../Layout/Layout";
-import { userData } from "./UserData";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 
 const Account = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userDataString = await AsyncStorage.getItem("user");
+        const userData = JSON.parse(userDataString);
+        setUserData(userData);
+        console.log(userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!userData) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <Layout>
       <View style={styles.container}>
         <View style={styles.userInfo}>
           <Image
-            source={{ uri: userData.userphoto }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+            }}
             style={styles.userPhoto}
           />
-          <Text style={styles.userName}>{userData.UserName}</Text>
-          <Text style={styles.fullname}>{userData.fullname}</Text>
-          <Text style={styles.email}>{userData.email}</Text>
-          <Text style={styles.password}>{userData.password}</Text>
+          <Text style={styles.userName}>{userData.name}</Text>
+          <TextInput
+            style={styles.input}
+            value={userData._id}
+            editable={false}
+          />
+          <TextInput
+            style={styles.input}
+            value={userData.email}
+            editable={false}
+          />
+          <TextInput
+            style={styles.input}
+            value={userData.password}
+            editable={false}
+          />
         </View>
       </View>
     </Layout>
@@ -47,17 +86,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: hp("1%"),
   },
-  fullname: {
-    fontSize: wp("4%"),
-    marginBottom: hp("1%"),
-  },
-  email: {
-    fontSize: wp("4%"),
-    color: "gray",
-    marginBottom: hp("1%"),
-  },
-  password: {
-    fontSize: wp("4%"),
+  input: {
+    width: wp("70%"),
+    height: hp("6%"),
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: wp("2%"),
     marginBottom: hp("1%"),
   },
 });
